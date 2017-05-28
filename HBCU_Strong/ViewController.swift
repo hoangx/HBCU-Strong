57949f4aab4ac046ce5b80ea2a4e121c6a771a13
Tommy Hoang <tommyhoang@Tommys-MacBook-Pro.local>
2017-05-28 06:55:01 -0500

    first commit

---
+++ HBCU_Strong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HBCU_Strong
+//
+//  Created by Tommy Hoang on 5/28/17.
+//  Copyright © 2017 Tommy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
