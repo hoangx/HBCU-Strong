e44d3e1ca62043cfc7ffa5d2b87c43aa73fa3904
Tommy Hoang <tommyhoang@Tommys-MacBook-Pro.local>
2017-05-28 08:09:14 -0500

    Created UI for sign up and login. Added Firebase as backend.

---
+++ HBCU_Strong/SignUpViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SignUpViewController.swift
+//  HBCU_Strong
+//
+//  Created by Tommy Hoang on 5/28/17.
+//  Copyright © 2017 Tommy Hoang. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func dismiss_OnClick(_ sender: Any) { (dismiss(animated: true, completion: nil))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'HBCU_Strong' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for HBCU_Strong
+  pod 'Firebase/Core'
+end
